b0c942c5963f0c39fe6db4fa139af85a04f5c3a5
RKrom <robin@getgreenshot.org>
2012-03-12 08:54:11 +0000

    Commit of missing files.
    git-svn-id: http://svn.code.sf.net/p/greenshot/code/trunk@1691 7dccd23d-a4a3-4e1f-8c07-b4c1b4018ab4

---
+++ GreenshotInterop/Interop/Base.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Greenshot.Interop {
+	/// <summary>
+	/// Common properties that has appreared in almost all objects
+	/// </summary>
+	public interface Common : IDisposable {
+	}
+}
---
+++ GreenshotInterop/Interop/IDispatch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.CustomMarshalers;
+
+namespace Greenshot.Interop {
+	[ComImport, Guid("00020400-0000-0000-C000-000000000046"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	public interface IDispatch {
+		void Reserved();
+		[PreserveSig]
+		int GetTypeInfo(uint nInfo, int lcid, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(TypeToTypeInfoMarshaler))] out Type typeInfo);
+	}
+}
