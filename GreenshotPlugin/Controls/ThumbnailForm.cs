5d742ce21d6866d72795b9fb62564dbad98e574c
RKrom <robin@getgreenshot.org>
2012-06-08 10:54:05 +0000

    Added license and comments
    git-svn-id: http://svn.code.sf.net/p/greenshot/code/trunk@1910 7dccd23d-a4a3-4e1f-8c07-b4c1b4018ab4

--- GreenshotPlugin/Controls/ThumbnailForm.cs
+++ GreenshotPlugin/Controls/ThumbnailForm.cs
@@ -1,6 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*
+ * Greenshot - a free and open source screenshot tool
+ * Copyright (C) 2007-2012  Thomas Braun, Jens Klingen, Robin Krom
+ * 
+ * For more information see: http://getgreenshot.org/
+ * The Greenshot project is hosted on Sourceforge: http://sourceforge.net/projects/greenshot/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Windows.Forms;
 using GreenshotPlugin.Core;
 using Greenshot.IniFile;
@@ -8,6 +26,10 @@ using System.Drawing;
 using GreenshotPlugin.UnmanagedHelpers;
 
 namespace GreenshotPlugin.Controls {
+    /// <summary>
+    /// This form allows us to show a Thumbnail preview of a window near the context menu when selecting a window to capture.
+    /// Didn't make it completely "generic" yet, but at least most logic is in here so we don't have it in the mainform.
+    /// </summary>
     public class ThumbnailForm : FormWithoutActivation {
         private static CoreConfiguration conf = IniConfig.GetIniSection<CoreConfiguration>();
 
@@ -43,6 +65,11 @@ namespace GreenshotPlugin.Controls {
             }
         }
 
+        /// <summary>
+        /// Show the thumbnail of the supplied window above (or under) the parent Control
+        /// </summary>
+        /// <param name="window">WindowDetails</param>
+        /// <param name="parentControl">Control</param>
         public void ShowThumbnail(WindowDetails window, Control parentControl) {
             UnregisterThumbnail();
 
