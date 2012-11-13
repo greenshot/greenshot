fc86a47220dc6d38fdd3d6b142103de12af7c768
RKrom <robin@getgreenshot.org>
2012-11-13 08:38:32 +0000

    Added Photobucket for Greenshot 1.1
    git-svn-id: http://svn.code.sf.net/p/greenshot/code/trunk@2274 7dccd23d-a4a3-4e1f-8c07-b4c1b4018ab4

---
+++ GreenshotPhotobucketPlugin/PhotobucketDestination.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Greenshot - a free and open source screenshot tool
+ * Copyright (C) 2007-2012  Thomas Braun, Jens Klingen, Robin Krom
+ * 
+ * For more information see: http://getgreenshot.org/
+ * The Greenshot project is hosted on Sourceforge: http://sourceforge.net/projects/greenshot/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System.ComponentModel;
+using System.Drawing;
+using Greenshot.IniFile;
+using Greenshot.Plugin;
+using GreenshotPlugin.Core;
+
+namespace GreenshotPhotobucketPlugin  {
+	/// <summary>
+	/// Description of PhotobucketDestination.
+	/// </summary>
+	public class PhotobucketDestination : AbstractDestination {
+		private static log4net.ILog LOG = log4net.LogManager.GetLogger(typeof(PhotobucketDestination));
+		private static PhotobucketConfiguration config = IniConfig.GetIniSection<PhotobucketConfiguration>();
+		private PhotobucketPlugin plugin = null;
+
+		public PhotobucketDestination(PhotobucketPlugin plugin) {
+			this.plugin = plugin;
+		}
+		
+		public override string Designation {
+			get {
+				return "Photobucket";
+			}
+		}
+
+		public override string Description {
+			get {
+				return Language.GetString("photobucket", LangKey.upload_menu_item);
+			}
+		}
+
+		public override Image DisplayIcon {
+			get {
+				ComponentResourceManager resources = new ComponentResourceManager(typeof(PhotobucketPlugin));
+				return (Image)resources.GetObject("Photobucket");
+			}
+		}
+
+		public override ExportInformation ExportCapture(bool manuallyInitiated, ISurface surface, ICaptureDetails captureDetails) {
+			ExportInformation exportInformation = new ExportInformation(this.Designation, this.Description);
+			using (Image image = surface.GetImageForExport()) {
+				string uploadURL = null;
+				bool uploaded = plugin.Upload(captureDetails, image, out uploadURL);
+				if (uploaded) {
+					exportInformation.ExportMade = true;
+					exportInformation.Uri = uploadURL;
+				}
+			}
+			ProcessExport(exportInformation, surface);
+			return exportInformation;
+		}
+	}
+}
