59d91bb501c7d53568c3ec101fca6fd216a7ce81
RKrom <robin@getgreenshot.org>
2012-11-19 10:41:56 +0000

    Make sure all exceptions in the thread are logged!
    git-svn-id: http://svn.code.sf.net/p/greenshot/code/trunk@2295 7dccd23d-a4a3-4e1f-8c07-b4c1b4018ab4

--- GreenshotPlugin/Controls/PleaseWaitForm.cs
+++ GreenshotPlugin/Controls/PleaseWaitForm.cs
@@ -77,6 +77,7 @@ namespace GreenshotPlugin.Controls {
 						try {
 							waitDelegate.Invoke();
 						} catch (Exception ex) {
+							LOG.Error("invoke error:", ex);
 							threadException = ex;
 						}
 					})
