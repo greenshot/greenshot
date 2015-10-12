a7c9bf8d3a7d5c1a646a9523befe20ac3f9654dd
Robin <robin@getgreenshot.org>
2015-10-12 17:43:42 +0200

    Made Greenshot a C# / WPF Project, added MenuWindow, AsyncCommand & DynamicMenuItem so the Destination picker can be reinvented. Updated the PleaseWaitWindow.xaml a bit... to reflect the style in the AboutWindow. [skip ci]

---
+++ Greenshot/Model/DynamicMenuItem.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * Greenshot - a free and open source screenshot tool
+ * Copyright (C) 2007-2015  Thomas Braun, Jens Klingen, Robin Krom
+ * 
+ * For more information see: http://getgreenshot.org/
+ * The Greenshot project is hosted on Sourceforge: http://sourceforge.net/projects/greenshot/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Greenshot.Model
+{
+	/// <summary>
+	/// Item for a menu, with sub items
+	/// </summary>
+	public class DynamicMenuItem : INotifyPropertyChanged
+	{
+		private string _text;
+		private string _shortcut;
+		private bool _isEnabled = true;
+		private ICommand _command;
+		private Image _icon;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Simple OnPropertyChanged implementation
+		/// </summary>
+		/// <param name="propertyName"></param>
+		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public string Shortcut
+		{
+			get
+			{
+				return _shortcut;
+			}
+			set
+			{
+				if (value != _shortcut)
+				{
+					_shortcut = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public string Text
+		{
+			get
+			{
+				return _text;
+			}
+			set
+			{
+				if (value != _text)
+				{
+					_text = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public bool IsEnabled
+		{
+			get
+			{
+				return _isEnabled;
+			}
+			set
+			{
+				if (value != _isEnabled)
+				{
+					_isEnabled = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public Image Icon
+		{
+			get
+			{
+				return _icon;
+			}
+			set
+			{
+				if (value != _icon)
+				{
+					_icon = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public ICommand Command
+		{
+			get
+			{
+				return _command;
+			}
+			set
+			{
+				if (value != _command)
+				{
+					_command = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public ObservableCollection<DynamicMenuItem> Children
+		{
+			get;
+			set;
+		} = new ObservableCollection<DynamicMenuItem>();
+	}
+}
---
+++ GreenshotPlugin/Core/AsyncCommand.cs
@@ -0,0 +1,104 @@
+﻿/*
+ * Greenshot - a free and open source screenshot tool
+ * Copyright (C) 2007-2015 Thomas Braun, Jens Klingen, Robin Krom
+ * 
+ * For more information see: http://getgreenshot.org/
+ * The Greenshot project is hosted on Sourceforge: http://sourceforge.net/projects/greenshot/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace GreenshotPlugin.Core
+{
+	public class AsyncCommand : ICommand
+	{
+		private readonly Predicate<object> _canExecute;
+		private readonly Func<object, Task> _asyncExecute;
+		private readonly bool _allowMultipleExecutionsAtOnce;
+
+		private bool _isExecuting;
+
+		public event EventHandler CanExecuteChanged;
+
+		public AsyncCommand(Func<object, Task> execute)
+		: this(execute, null, false)
+		{
+		}
+
+		public AsyncCommand(Func<object, Task> execute, bool allowMultipleExecutionsAtOnce = false)
+		: this(execute, null, allowMultipleExecutionsAtOnce)
+		{
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="asyncExecute">Function for the execute</param>
+		/// <param name="canExecute">Predicate for the canExecute</param>
+		/// <param name="allowMultipleExecutionsAtOnce">Whether or not this command can be executed several times in a row, or if it must wait until the previous command has been executed. Defaults to false.</param>
+		public AsyncCommand(Func<object, Task> asyncExecute, Predicate<object> canExecute, bool allowMultipleExecutionsAtOnce = false)
+		{
+			_asyncExecute = asyncExecute;
+			_canExecute = canExecute;
+			_allowMultipleExecutionsAtOnce = allowMultipleExecutionsAtOnce;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			if (!_allowMultipleExecutionsAtOnce && _isExecuting)
+			{
+				return false;
+			}
+
+			if (_canExecute == null)
+			{
+				return true;
+			}
+
+			return _canExecute(parameter);
+		}
+
+		public async void Execute(object parameter)
+		{
+			_isExecuting = true;
+			UpdateCanExecuteState();
+			await ExecuteAsync(parameter);
+			_isExecuting = false;
+			UpdateCanExecuteState();
+		}
+
+		protected virtual async Task ExecuteAsync(object parameter)
+		{
+			if (_asyncExecute != null)
+			{
+				await _asyncExecute(parameter);
+			}
+		}
+
+		public void UpdateCanExecuteState()
+		{
+			if (CanExecuteChanged != null)
+			{
+				Dispatcher.CurrentDispatcher.BeginInvoke((Action)(() => CanExecuteChanged(this, new EventArgs())));
+			}
+		}
+	}
+
+}
