1294036a568ad5e3f3a22da1b42c73daae35a4a8
Robin <robin@getgreenshot.org>
2016-08-17 14:38:41 +0200

    Fixed copyright for this year.

--- GreenshotDropboxPlugin/Forms/DropboxForm.cs
+++ GreenshotDropboxPlugin/Forms/DropboxForm.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Greenshot - a free and open source screenshot tool
- * Copyright (C) 2007-2015 Thomas Braun, Jens Klingen, Robin Krom, Francis Noel
+ * Copyright (C) 2007-2016 Thomas Braun, Jens Klingen, Robin Krom, Francis Noel
  * 
  * For more information see: http://getgreenshot.org/
  * The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
