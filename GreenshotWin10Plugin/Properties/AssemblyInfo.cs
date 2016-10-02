85e8f763114ce8347ce6fa9964125ec13e9cd894
Robin <robin@getgreenshot.org>
2016-10-02 16:34:26 +0200

    Fixed the fact that the plug-in wasn't visible in the settings.

--- GreenshotWin10Plugin/Properties/AssemblyInfo.cs
+++ GreenshotWin10Plugin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GreenshotWin10Plugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A plug-in for Windows 10 only functionality")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Greenshot")]
 [assembly: AssemblyProduct("GreenshotWin10Plugin")]
