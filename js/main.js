29477c0d304365bc81774cfc637f1b2513e2975b
jklingen <jens@getgreenshot.org>
2017-01-07 18:43:54 +0100

    Alternative ads

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+// Function called if AdBlock is detected
+function adBlockDetected() {
+    $('.ga-ldrbrd').append('<a href="/support-us/"><img src="/assets/bab-banner.png"></a>');
+    $('.ga-skscrpr').append('<a href="/support-us/"><img src="/assets/bab-skyscraper.png"></a>');
+    $('.ga-skscrpr-wrapper').hide();
+}
+
+// Recommended audit because AdBlock lock the file 'blockadblock.js' 
+// If the file is not called, the variable does not exist 'blockAdBlock'
+// This means that AdBlock is present
+if(typeof blockAdBlock === 'undefined') {
+    adBlockDetected();
+} else {
+    blockAdBlock.onDetected(adBlockDetected);
+    blockAdBlock.onNotDetected(adBlockNotDetected);
+}	
