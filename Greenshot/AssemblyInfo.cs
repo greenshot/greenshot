a0866aec316714d28d8843f291deb83feab62af0
xucongli1989 <80213876@qq.com>
2017-01-21 16:54:29 +0800

    update AssemblyInfo.cs

--- Greenshot/AssemblyInfo.cs
+++ Greenshot/AssemblyInfo.cs
@@ -25,12 +25,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Greenshot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Greenshot 屏幕截图软件——XCL修改版")]
+[assembly: AssemblyDescription("Greenshot 屏幕截图软件——XCL修改版")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Greenshot")]
 [assembly: AssemblyProduct("Greenshot")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("原版：https://github.com/greenshot/greenshot，修改版：https://github.com/xucongli1989/greenshot")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -39,6 +39,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The assembly version, replaced by build scripts
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0")]
