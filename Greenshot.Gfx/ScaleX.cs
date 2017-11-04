cf7a3be2ae3a14267fdb8b4ee5dc3153f6d68494
Robin <robin@dapplo.net>
2017-11-04 21:18:42 +0100

    Fixed an issue with the Scale2X routine, also updated Dapplo.Windows so the scrolling works for Directory Opus.

---
+++ Greenshot.Gfx/ScaleX.cs
@@ -0,0 +1,194 @@
+﻿#region Greenshot GNU General Public License
+
+// Greenshot - a free and open source screenshot tool
+// Copyright (C) 2007-2017 Thomas Braun, Jens Klingen, Robin Krom
+// 
+// For more information see: http://getgreenshot.org/
+// The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 1 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Greenshot.Gfx.FastBitmap;
+
+namespace Greenshot.Gfx
+{
+    public static class ScaleX
+    {
+        /// <summary>
+        ///     Use "Scale2x" algorithm to produce bitmap from the original.
+        /// </summary>
+        /// <param name="original">Bitmap to scale 2x</param>
+        public static Bitmap Scale2X(this Bitmap original)
+        {
+            using (var source = (IFastBitmapWithClip)FastBitmapFactory.Create(original))
+            using (var destination = (IFastBitmapWithClip)FastBitmapFactory.CreateEmpty(new Size(original.Width << 1, original.Height << 1), original.PixelFormat))
+            {
+                // Every pixel from input texture produces 4 output pixels, for more details check out http://scale2x.sourceforge.net/algorithm.html
+                Parallel.For(0, source.Height, y =>
+                {
+                    unsafe
+                    {
+                        var colorB = stackalloc byte[4];
+                        var colorD = stackalloc byte[4];
+                        var colorE = stackalloc byte[4];
+                        var colorF = stackalloc byte[4];
+                        var colorH = stackalloc byte[4];
+                        var x = 0;
+                        while (x < source.Width)
+                        {
+                            source.GetColorAt(x, y - 1, colorB);
+                            source.GetColorAt(x, y + 1, colorH);
+                            source.GetColorAt(x - 1, y, colorD);
+                            source.GetColorAt(x + 1, y, colorF);
+                            source.GetColorAt(x, y, colorE);
+
+                            byte* colorE0, colorE1, colorE2, colorE3;
+                            if (!AreColorsSame(colorB, colorH) && !AreColorsSame(colorD, colorF))
+                            {
+                                colorE0 = AreColorsSame(colorD, colorB) ? colorD : colorE;
+                                colorE1 = AreColorsSame(colorB, colorF) ? colorF : colorE;
+                                colorE2 = AreColorsSame(colorD, colorH) ? colorD : colorE;
+                                colorE3 = AreColorsSame(colorH, colorF) ? colorF : colorE;
+                            }
+                            else
+                            {
+                                colorE0 = colorE;
+                                colorE1 = colorE;
+                                colorE2 = colorE;
+                                colorE3 = colorE;
+                            }
+
+                            destination.SetColorAt(x << 1, y << 1, colorE0);
+                            destination.SetColorAt((x << 1) + 1, y << 1, colorE1);
+                            destination.SetColorAt(x << 1, (y << 1) + 1, colorE2);
+                            destination.SetColorAt((x << 1) + 1, (y << 1) + 1, colorE3);
+
+                            x++;
+                        }
+
+                    }
+                });
+                return destination.UnlockAndReturnBitmap();
+            }
+        }
+
+        /// <summary>
+        ///     Use "Scale3x" algorithm to produce bitmap from the original.
+        /// </summary>
+        /// <param name="original">Bitmap to scale 3x</param>
+        [SuppressMessage("ReSharper", "AccessToDisposedClosure")]
+        public static Bitmap Scale3X(this Bitmap original)
+        {
+            using (var source = (IFastBitmapWithClip)FastBitmapFactory.Create(original))
+            using (var destination = (IFastBitmapWithClip)FastBitmapFactory.CreateEmpty(new Size(original.Width * 3, original.Height * 3), original.PixelFormat))
+            {
+                // Every pixel from input texture produces 6 output pixels, for more details check out http://scale2x.sourceforge.net/algorithm.html
+                Parallel.For(0, source.Height, y =>
+                {
+                    unsafe
+                    {
+                        var x = 0;
+                        var colorA = stackalloc byte[4];
+                        var colorB = stackalloc byte[4];
+                        var colorC = stackalloc byte[4];
+                        var colorD = stackalloc byte[4];
+                        var colorE = stackalloc byte[4];
+                        var colorF = stackalloc byte[4];
+                        var colorG = stackalloc byte[4];
+                        var colorH = stackalloc byte[4];
+                        var colorI = stackalloc byte[4];
+                        while (x < source.Width)
+                        {
+                            source.GetColorAt(x - 1, y - 1, colorA);
+                            source.GetColorAt(x, y - 1, colorB);
+                            source.GetColorAt(x + 1, y - 1, colorC);
+
+                            source.GetColorAt(x - 1, y, colorD);
+                            source.GetColorAt(x, y, colorE);
+                            source.GetColorAt(x + 1, y, colorF);
+
+                            source.GetColorAt(x - 1, y + 1, colorG);
+                            source.GetColorAt(x, y + 1, colorH);
+                            source.GetColorAt(x + 1, y + 1, colorI);
+
+                            byte* colorE0, colorE1, colorE2, colorE3, colorE4, colorE5, colorE6, colorE7, colorE8;
+
+                            if (!AreColorsSame(colorB, colorH) && !AreColorsSame(colorD, colorF))
+                            {
+                                colorE0 = AreColorsSame(colorD, colorB) ? colorD : colorE;
+                                colorE1 = AreColorsSame(colorD, colorB) && !AreColorsSame(colorE, colorC) || AreColorsSame(colorB, colorF) && !AreColorsSame(colorE, colorA) ? colorB : colorE;
+                                colorE2 = AreColorsSame(colorB, colorF) ? colorF : colorE;
+                                colorE3 = AreColorsSame(colorD, colorB) && !AreColorsSame(colorE, colorG) || AreColorsSame(colorD, colorH) && !AreColorsSame(colorE, colorA) ? colorD : colorE;
+
+                                colorE4 = colorE;
+                                colorE5 = AreColorsSame(colorB, colorF) && !AreColorsSame(colorE, colorI) || AreColorsSame(colorH, colorF) && !AreColorsSame(colorE, colorC) ? colorF : colorE;
+                                colorE6 = AreColorsSame(colorD, colorH) ? colorD : colorE;
+                                colorE7 = AreColorsSame(colorD, colorH) && !AreColorsSame(colorE, colorI) || AreColorsSame(colorH, colorF) && !AreColorsSame(colorE, colorG) ? colorH : colorE;
+                                colorE8 = AreColorsSame(colorH, colorF) ? colorF : colorE;
+                            }
+                            else
+                            {
+                                colorE0 = colorE;
+                                colorE1 = colorE;
+                                colorE2 = colorE;
+                                colorE3 = colorE;
+                                colorE4 = colorE;
+                                colorE5 = colorE;
+                                colorE6 = colorE;
+                                colorE7 = colorE;
+                                colorE8 = colorE;
+                            }
+                            var multipliedX = 3 * x;
+                            var multipliedY = 3 * y;
+
+                            destination.SetColorAt(multipliedX - 1, multipliedY - 1, colorE0);
+                            destination.SetColorAt(multipliedX, multipliedY - 1, colorE1);
+                            destination.SetColorAt(multipliedX + 1, multipliedY - 1, colorE2);
+
+                            destination.SetColorAt(multipliedX - 1, multipliedY, colorE3);
+                            destination.SetColorAt(multipliedX, multipliedY, colorE4);
+                            destination.SetColorAt(multipliedX + 1, multipliedY, colorE5);
+
+                            destination.SetColorAt(multipliedX - 1, multipliedY + 1, colorE6);
+                            destination.SetColorAt(multipliedX, multipliedY + 1, colorE7);
+                            destination.SetColorAt(multipliedX + 1, multipliedY + 1, colorE8);
+
+                            x++;
+                        }
+
+                    }
+                });
+                return destination.UnlockAndReturnBitmap();
+            }
+        }
+
+        /// <summary>
+        ///     Checks if the colors are the same.
+        /// </summary>
+        /// <param name="aColor">Color first</param>
+        /// <param name="bColor">Color second</param>
+        /// <returns>True if they are; otherwise false</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe bool AreColorsSame(byte* aColor, byte* bColor)
+        {
+            return aColor[0] == bColor[0] && aColor[1] == bColor[1] && aColor[2] == bColor[2] && aColor[3] == bColor[3];
+        }
+    }
+}
---
+++ Greenshot/Helpers/ScrollingCapture.cs
@@ -0,0 +1,163 @@
+﻿#region Greenshot GNU General Public License
+
+// Greenshot - a free and open source screenshot tool
+// Copyright (C) 2007-2017 Thomas Braun, Jens Klingen, Robin Krom
+// 
+// For more information see: http://getgreenshot.org/
+// The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 1 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using System.Drawing;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Dapplo.Log;
+using Dapplo.Windows.Common.Structs;
+using Dapplo.Windows.Desktop;
+using Dapplo.Windows.Input;
+using Dapplo.Windows.Input.Enums;
+using Dapplo.Windows.User32;
+using Dapplo.Windows.User32.Enums;
+using Greenshot.Gfx.Stitching;
+using GreenshotPlugin.Core;
+
+namespace Greenshot.Helpers
+{
+    /// <summary>
+    /// This takes care of taking a scrolling capture
+    /// </summary>
+    public class ScrollingCapture
+    {
+        private static readonly LogSource Log = new LogSource();
+        private readonly WindowScroller _windowScroller;
+
+        public ScrollingCapture(WindowScroller windowScroller)
+        {
+            _windowScroller = windowScroller;
+
+            // Set scrollmode to windows message, which is the default but still...
+            windowScroller.ScrollMode = ScrollModes.WindowsMessage;
+        }
+
+        /// <summary>
+        /// Start the capture
+        /// </summary>
+        /// <returns>Bitmap</returns>
+        public Bitmap Capture()
+        {
+            if (_windowScroller.NeedsFocus())
+            {
+                User32Api.SetForegroundWindow(_windowScroller.ScrollBarWindow.Handle);
+                Application.DoEvents();
+                Thread.Sleep(100);
+                Application.DoEvents();
+            }
+
+            // Find the area which is scrolling
+
+            // 1. Take the client bounds
+            var clientBounds = _windowScroller.ScrollBarWindow.GetInfo().ClientBounds;
+
+            // Use a region for steps 2 and 3
+            using (var region = new Region(clientBounds))
+            {
+                // 2. exclude the children, if any
+                foreach (var interopWindow in _windowScroller.ScrollBarWindow.GetChildren())
+                {
+                    region.Exclude(interopWindow.GetInfo().Bounds);
+                }
+                // 3. exclude the scrollbar, if it can be found
+                if (_windowScroller.ScrollBar.HasValue)
+                {
+                    region.Exclude(_windowScroller.ScrollBar.Value.Bounds);
+                }
+                // Get the bounds of the region
+                using (var screenGraphics = Graphics.FromHwnd(User32Api.GetDesktopWindow()))
+                {
+                    var rectangleF = region.GetBounds(screenGraphics);
+                    clientBounds = new NativeRect((int)rectangleF.X, (int)rectangleF.Y, (int)rectangleF.Width, (int)rectangleF.Height);
+                }
+            }
+
+            if (clientBounds.Width * clientBounds.Height > 0)
+            {
+                // Move the window to the start
+                _windowScroller.Start();
+
+                // Register a keyboard hook to make it possible to ESC the capturing
+                var breakScroll = false;
+                var keyboardHook = KeyboardHook.KeyboardEvents
+                    .Where(args => args.Key == VirtualKeyCodes.ESCAPE)
+                    .Subscribe(args =>
+                    {
+                        args.Handled = true;
+                        breakScroll = true;
+                    });
+                Bitmap resultImage = null;
+                try
+                {
+                    // A delay to make the window move
+                    Application.DoEvents();
+                    Thread.Sleep(100);
+                    Application.DoEvents();
+
+                    if (_windowScroller.IsAtStart)
+                    {
+                        using (var bitmapStitcher = new BitmapStitcher())
+                        {
+                            bitmapStitcher.AddBitmap(WindowCapture.CaptureRectangle(clientBounds));
+
+                            // Loop as long as we are not at the end yet
+                            while (!_windowScroller.IsAtEnd && !breakScroll)
+                            {
+                                // Next "page"
+                                _windowScroller.Next();
+                                // Wait a bit, so the window can update
+                                Application.DoEvents();
+                                Thread.Sleep(100);
+                                Application.DoEvents();
+                                // Capture inside loop
+                                bitmapStitcher.AddBitmap(WindowCapture.CaptureRectangle(clientBounds));
+                            }
+                            resultImage = bitmapStitcher.Result();
+                        }
+
+                    }
+                    else
+                    {
+                        resultImage = WindowCapture.CaptureRectangle(clientBounds);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error().WriteLine(ex);
+                }
+                finally
+                {
+                    // Remove hook for escape
+                    keyboardHook.Dispose();
+                    // Try to reset location
+                    _windowScroller.Reset();
+                }
+                
+                return resultImage;
+            }
+            return null;
+        }
+    }
+}
