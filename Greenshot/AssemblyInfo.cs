73713cdccd59ee4cdc1186d70ec3b59744dbb1ae
xucongli1989 <x889@foxmail.com>
2018-06-03 12:40:23 +0800

    Remove unused plugin.

--- Greenshot/AssemblyInfo.cs
+++ Greenshot/AssemblyInfo.cs
@@ -1,20 +1,20 @@
 ﻿/*
  * Greenshot - a free and open source screenshot tool
  * Copyright (C) 2007-2016 Thomas Braun, Jens Klingen, Robin Krom
- * 
+ *
  * For more information see: http://getgreenshot.org/
  * The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 1 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,7 +22,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Greenshot 屏幕截图软件——XCL修改版（80213876@qq.com）")]
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The assembly version, replaced by build scripts
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyInformationalVersion("1.1")]
+[assembly: AssemblyVersion("1.2.10")]
+[assembly: AssemblyInformationalVersion("1.2.10")]
