2404bafa603206b4134d93559d95f5b6986fe355
Robin Krom <robin.krom@gmail.com>
2019-02-25 22:54:05 +0100

    Some small documentation fix, and fixed an issue with not being able to run BenchmarkDotNet while PdbGit is loaded. (Caused because the project name does not contain test)

--- src/Greenshot.Addon.Imgur/Entities/ImgurData.cs
+++ src/Greenshot.Addon.Imgur/Entities/ImgurData.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿#region Greenshot GNU General Public License
+
+// Greenshot - a free and open source screenshot tool
+// Copyright (C) 2007-2018 Thomas Braun, Jens Klingen, Robin Krom
+// 
+// For more information see: http://getgreenshot.org/
+// The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 1 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -10,33 +33,63 @@ namespace Greenshot.Addon.Imgur.Entities
     /// </summary>
     public class ImgurData
     {
+        /// <summary>
+        /// Id of the image
+        /// </summary>
         [JsonProperty("id")]
         public string Id { get; set; }
 
+        /// <summary>
+        /// Title of the image
+        /// </summary>
         [JsonProperty("title")]
         public string Title { get; set; }
 
+        /// <summary>
+        /// Description of the image
+        /// </summary>
         [JsonProperty("description")]
         public string Description { get; set; }
 
+        /// <summary>
+        /// Date created
+        /// </summary>
         [JsonProperty("datetime"), JsonConverter(typeof(UnixDateTimeConverter))]
         public DateTime Datetime { get; set; }
 
+        /// <summary>
+        /// Type of image
+        /// </summary>
         [JsonProperty("type")]
         public string Type { get; set; }
 
+        /// <summary>
+        /// Specifies if the image is animated
+        /// </summary>
         [JsonProperty("animated")]
         public bool Animated { get; set; }
 
+        /// <summary>
+        /// The width of the image
+        /// </summary>
         [JsonProperty("width")]
         public long Width { get; set; }
 
+        /// <summary>
+        /// The height of the image
+        /// </summary>
         [JsonProperty("height")]
         public long Height { get; set; }
 
+        /// <summary>
+        /// The size of the image
+        /// </summary>
         [JsonProperty("size")]
         public long Size { get; set; }
 
+        /// <summary>
+        /// How many times is this image viewed
+        /// </summary>
         [JsonProperty("views")]
         public long Views { get; set; }
 
