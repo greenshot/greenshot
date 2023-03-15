4524e2511e1b0aa65ef74c0acde74a9c8c9d487f
Nathan Brown <nathanofbrown@gmail.com>
2023-03-14 19:51:56 -0700

    Revert "Remove unused parameters"
    This reverts commit 1d862d6f9fc8690fd121d54fe8770b10755be75f.

--- src/Greenshot.Editor/Drawing/Surface.cs
+++ src/Greenshot.Editor/Drawing/Surface.cs
@@ -2136,7 +2136,7 @@ namespace Greenshot.Editor.Drawing
             }
             else
             {
-                ConfirmSelectedConfirmableElements();
+                ConfirmSelectedConfirmableElements(confirm);
             }
         }
 
@@ -2147,7 +2147,7 @@ namespace Greenshot.Editor.Drawing
         /// For crop-container there is a dedicated method <see cref="ConfirmCrop(bool)"/>.
         /// </summary>
         /// <param name="confirm">bool</param>
-        public void ConfirmSelectedConfirmableElements()
+        public void ConfirmSelectedConfirmableElements(bool confirm)
         {
             // create new collection so that we can iterate safely (selectedElements might change due with confirm/cancel)
             List<IDrawableContainer> selectedDCs = new List<IDrawableContainer>(selectedElements);
