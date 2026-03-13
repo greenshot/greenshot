Lakritzator/greenshot-refactorings

commit 1f67eee9389d42e850408923fafb79933297d8fd

Add safety checks, locking, and sync fixes (#1027)
[skip ci]

src/Greenshot.Editor/Forms/ImageEditorForm.cs
@@ -71,6 +71,7 @@ namespace Greenshot.Editor.Forms
         };
 
         private static readonly List<IImageEditor> EditorList = new();
+        private static readonly object _editorListLock = new();
 
         private Surface _surface;
         private GreenshotToolStripButton[] _toolbarButtons;
@@ -101,16 +102,19 @@ namespace Greenshot.Editor.Forms
         {
             get
             {
-                try
+                lock (_editorListLock)
                 {
-                    EditorList.Sort((e1, e2) => string.Compare(e1.Surface.CaptureDetails.Title, e2.Surface.CaptureDetails.Title, StringComparison.Ordinal));
-                }
-                catch (Exception ex)
-                {
-                    Log.Warn("Sorting of editors failed.", ex);
-                }
+                    try
+                    {
+                        EditorList.Sort((e1, e2) => string.Compare(e1.Surface.CaptureDetails.Title, e2.Surface.CaptureDetails.Title, StringComparison.Ordinal));
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Warn("Sorting of editors failed.", ex);
+                    }
 
-                return EditorList;
+                    return EditorList;
+                }
             }
         }
 
@@ -148,7 +152,10 @@ namespace Greenshot.Editor.Forms
             // Compute emojis in background
             EmojiData.Load();
 
-            EditorList.Add(this);
+            lock (_editorListLock)
+            {
+                EditorList.Add(this);
+            }
 
             //
             // The InitializeComponent() call is required for Windows Forms designer support.
@@ -1015,7 +1022,10 @@ namespace Greenshot.Editor.Forms
             IniConfig.Save();
 
             // remove from the editor list
-            EditorList.Remove(this);
+            lock (_editorListLock)
+            {
+                EditorList.Remove(this);
+            }
 
             _surface.Dispose();
 

src/Greenshot.Base/Core/Fraction.cs
@@ -82,7 +82,7 @@ namespace Greenshot.Base.Core
         }
 
         public int CompareTo(Fraction other)
-            => (int) (Numerator * other.Denominator) - (int) (other.Numerator * Denominator);
+            => ((long) Numerator * other.Denominator).CompareTo((long) other.Numerator * Denominator);
 
         #endregion
 

src/Greenshot.Base/Core/ImageHelper.cs
@@ -74,6 +74,10 @@ namespace Greenshot.Base.Core
                 }
 
                 PropertyItem item = image.GetPropertyItem(ExifOrientationId);
+                if (item.Value == null || item.Value.Length == 0)
+                {
+                    return;
+                }
 
                 ExifOrientations orientation = (ExifOrientations) item.Value[0];
                 // Orient the image.
@@ -1249,7 +1253,12 @@ namespace Greenshot.Base.Core
             else
             {
                 // Let GDI+ decide how to convert, need to test what is quicker...
-                newImage = (sourceImage as Bitmap).Clone(sourceRect, targetFormat);
+                var sourceBitmap = sourceImage as Bitmap;
+                if (sourceBitmap == null)
+                {
+                    throw new InvalidOperationException($"Expected Bitmap but got {sourceImage?.GetType().Name}");
+                }
+                newImage = sourceBitmap.Clone(sourceRect, targetFormat);
                 // Make sure both images have the same resolution
                 newImage.SetResolution(sourceImage.HorizontalResolution, sourceImage.VerticalResolution);
             }

src/Greenshot.Base/Core/NetworkHelper.cs
@@ -116,6 +116,7 @@ namespace Greenshot.Base.Core
                 catch (Exception)
                 {
                     // If we arrive here, the image loading didn't work, try to see if the response has a http(s) URL to an image and just take this instead.
+                    memoryStream.Seek(0, SeekOrigin.Begin);
                     string content;
                     using (var streamReader = new StreamReader(memoryStream, Encoding.UTF8, true))
                     {
@@ -177,6 +178,7 @@ namespace Greenshot.Base.Core
                 catch (Exception)
                 {
                     // If we arrive here, the image loading didn't work, try to see if the response has a http(s) URL to an image and just take this instead.
+                    memoryStream.Seek(0, SeekOrigin.Begin);
                     string content;
                     using (var streamReader = new StreamReader(memoryStream, Encoding.UTF8, true))
                     {

src/Greenshot.Editor/Helpers/ScaleHelper.cs
@@ -243,8 +243,8 @@ namespace Greenshot.Editor.Helpers
 
             if (centeredScale)
             {
-                float wdiff = result.Width - result.Width;
-                float hdiff = result.Height - result.Height;
+                float wdiff = (result.Width - boundsBeforeResize.Width) / 2;
+                float hdiff = (result.Height - boundsBeforeResize.Height) / 2;
                 result = result.Inflate(wdiff, hdiff);
             }
 

src/Greenshot.Plugin.Confluence/Confluence.cs
@@ -96,8 +96,7 @@ public class ConfluenceConnector : IDisposable
             _confluence.SetBasicAuthentication(user, password);
             
             // Test the credentials by getting current user info
-            var testTask = Task.Run(async () => await _confluence.User.GetCurrentUserAsync().ConfigureAwait(false));
-            testTask.Wait();
+            Task.Run(async () => await _confluence.User.GetCurrentUserAsync().ConfigureAwait(false)).GetAwaiter().GetResult();
             
             _loggedInTime = DateTime.Now;
             _loggedIn = true;
@@ -200,13 +199,11 @@ public class ConfluenceConnector : IDisposable
     {
         CheckCredentials();
         
-        var attachTask = Task.Run(async () =>
+        Task.Run(async () =>
         {
             using var stream = new System.IO.MemoryStream(image.ToByteArray());
             await _confluence.Attachment.AttachAsync(pageId, stream, filename, comment, mime).ConfigureAwait(false);
-        });
-        
-        attachTask.Wait();
+        }).GetAwaiter().GetResult();
     }
 
     public Entities.Page GetPage(string spaceKey, string pageTitle)
@@ -222,7 +219,7 @@ public class ConfluenceConnector : IDisposable
         {
             CheckCredentials();
             
-            var pageTask = Task.Run(async () =>
+            page = Task.Run(async () =>
             {
                 var query = Where.And(Where.Type.IsPage, Where.Title.Is(pageTitle), Where.Space.Is(spaceKey));
                 var searchResult = await _confluence.Content.SearchAsync(query, pagingInformation: new PagingInformation
@@ -230,16 +227,13 @@ public class ConfluenceConnector : IDisposable
                     Limit = 1
                 }).ConfigureAwait(false);
                 return searchResult.Results.FirstOrDefault();
-            });
-            
-            page = pageTask.Result;
-            
+            }).GetAwaiter().GetResult();
+
             if (page != null)
             {
                 // Get full page details with body
-                var detailTask = Task.Run(async () => 
-                    await _confluence.Content.GetAsync(page, ConfluenceClientConfig.ExpandGetContentWithStorage).ConfigureAwait(false));
-                page = detailTask.Result;
+                page = Task.Run(async () =>
+                    await _confluence.Content.GetAsync(page, ConfluenceClientConfig.ExpandGetContentWithStorage).ConfigureAwait(false)).GetAwaiter().GetResult();
                 _pageCache.Add(cacheKey, page);
             }
         }
@@ -261,10 +255,8 @@ public class ConfluenceConnector : IDisposable
         {
             CheckCredentials();
             
-            var pageTask = Task.Run(async () =>
-                await _confluence.Content.GetAsync(pageId, ConfluenceClientConfig.ExpandGetContentWithStorage).ConfigureAwait(false));
-                
-            page = pageTask.Result;
+            page = Task.Run(async () =>
+                await _confluence.Content.GetAsync(pageId, ConfluenceClientConfig.ExpandGetContentWithStorage).ConfigureAwait(false)).GetAwaiter().GetResult();
             _pageCache.Add(cacheKey, page);
         }
 
@@ -275,7 +267,7 @@ public class ConfluenceConnector : IDisposable
     {
         CheckCredentials();
         
-        var homePageTask = Task.Run(async () =>
+        var page = Task.Run(async () =>
         {
             var space = await _confluence.Space.GetAsync(spaceSummary.Key).ConfigureAwait(false);
             if (space.HomepageId != 0)
@@ -283,9 +275,7 @@ public class ConfluenceConnector : IDisposable
                 return await _confluence.Content.GetAsync(space.HomepageId, ConfluenceClientConfig.ExpandGetContentWithStorage).ConfigureAwait(false);
             }
             return null;
-        });
-        
-        var page = homePageTask.Result;
+        }).GetAwaiter().GetResult();
         return page != null ? new Entities.Page(page) : null;
     }
 
@@ -293,20 +283,18 @@ public class ConfluenceConnector : IDisposable
     {
         CheckCredentials();
         
-        var spacesTask = Task.Run(async () =>
+        var spaces = Task.Run(async () =>
         {
             var allSpaces = new List<Dapplo.Confluence.Entities.Space>();
             var spacesResult = await _confluence.Space.GetAllAsync().ConfigureAwait(false);
-            
+
             foreach (var space in spacesResult)
             {
                 allSpaces.Add(space);
             }
-            
+
             return allSpaces;
-        });
-        
-        var spaces = spacesTask.Result;
+        }).GetAwaiter().GetResult();
         
         foreach (var space in spaces)
         {
@@ -318,20 +306,18 @@ public class ConfluenceConnector : IDisposable
     {
         CheckCredentials();
         
-        var childrenTask = Task.Run(async () =>
+        var pages = Task.Run(async () =>
         {
             var children = new List<Content>();
             var childrenResult = await _confluence.Content.GetChildrenAsync(parentPage.Id).ConfigureAwait(false);
-            
+
             if (childrenResult?.Results != null)
             {
                 children.AddRange(childrenResult.Results);
             }
-            
+
             return children;
-        });
-        
-        var pages = childrenTask.Result;
+        }).GetAwaiter().GetResult();
         
         foreach (var page in pages)
         {
@@ -343,21 +329,19 @@ public class ConfluenceConnector : IDisposable
     {
         CheckCredentials();
         
-        var pagesTask = Task.Run(async () =>
+        var pages = Task.Run(async () =>
         {
             var allPages = new List<Content>();
             var query = Where.And(Where.Type.IsPage, Where.Space.Is(space.Key));
             var searchResult = await _confluence.Content.SearchAsync(query).ConfigureAwait(false);
-            
+
             foreach (var page in searchResult.Results)
             {
                 allPages.Add(page);
             }
-            
+
             return allPages;
-        });
-        
-        var pages = pagesTask.Result;
+        }).GetAwaiter().GetResult();
         
         foreach (var page in pages)
         {
@@ -369,11 +353,11 @@ public class ConfluenceConnector : IDisposable
     {
         CheckCredentials();
         
-        var searchTask = Task.Run(async () =>
+        var pages = Task.Run(async () =>
         {
             var allPages = new List<Content>();
             IFinalClause whereClause;
-            
+
             if (!string.IsNullOrEmpty(space))
             {
                 whereClause = Where.And(Where.Type.IsPage, Where.Text.Contains(query), Where.Space.Is(space));
@@ -382,9 +366,9 @@ public class ConfluenceConnector : IDisposable
             {
                 whereClause = Where.And(Where.Type.IsPage, Where.Text.Contains(query));
             }
-            
+
             var searchResult = await _confluence.Content.SearchAsync(whereClause, pagingInformation: new PagingInformation { Limit = 20 }).ConfigureAwait(false);
-            
+
             foreach (var page in searchResult.Results)
             {
                 Log.DebugFormat("Got result of type {0}", page.Type);
@@ -393,11 +377,9 @@ public class ConfluenceConnector : IDisposable
                     allPages.Add(page);
                 }
             }
-            
+
             return allPages;
-        });
-        
-        var pages = searchTask.Result;
+        }).GetAwaiter().GetResult();
         
         foreach (var page in pages)
         {

src/Greenshot.Plugin.Office/OfficeExport/WordExporter.cs
@@ -86,9 +86,14 @@ namespace Greenshot.Plugin.Office.OfficeExport
             {
                 wordApplication = OleAut32Api.GetActiveObject<Application>("Word.Application");
             }
-            catch (Exception)
+            catch (System.Runtime.InteropServices.COMException)
             {
-                // Ignore, probably no word running
+                // Word is not running, this is expected
+                return null;
+            }
+            catch (Exception ex)
+            {
+                LOG.Warn("Unexpected error while getting Word application instance.", ex);
                 return null;
             }
 

src/Greenshot/Forms/MainForm.cs
@@ -803,13 +803,18 @@ namespace Greenshot.Forms
             }
 
             var oneDriveSettingsFile = Directory.GetFiles(oneDriveSettingsPath, "*_screenshot.dat").FirstOrDefault();
-            if (!File.Exists(oneDriveSettingsFile))
+            if (oneDriveSettingsFile == null || !File.Exists(oneDriveSettingsFile))
             {
                 return false;
             }
 
-            var screenshotSetting = File.ReadAllLines(oneDriveSettingsFile).Skip(1).Take(1).First();
-            return "2".Equals(screenshotSetting);
+            var lines = File.ReadAllLines(oneDriveSettingsFile);
+            if (lines.Length < 2)
+            {
+                return false;
+            }
+
+            return "2".Equals(lines[1]);
         }
 
         /// <summary>

greenshot/greenshot

commit 8b968ea8a5f5f58311009afe1c1746b3e23de3c7

Fixing my mistakes
This fix addresses two bugs introduced in PR #1027 that affected the image editor. The first was a double-registration issue in ImageEditorForm.Initialize() — when a new editor opened, it was being added to the global EditorList twice (once by SetSurface() and again explicitly in Initialize()). Since List.Remove() only removes the first occurrence, closing an editor left a ghost entry behind, causing the "Open in image editor" destination picker submenu to accumulate stale closed-editor entries indefinitely. The second bug was that UpdateUi() → ApplyLanguage() was overwriting the editor window title (set correctly by SetSurface()) with just the bare "Greenshot editor" string on every new editor open — this has been fixed by re-applying the capture title after UpdateUi() completes. Additionally, the submenu entries targeting a specific open editor have been renamed from "Open in image editor" to "Add to editor....." for clarity, since selecting one of those entries adds the new screenshot as an image object into an already-open editor rather than opening a fresh one.

Apologies for the disruption — both root causes are now properly resolved!

Fixes https://github.com/greenshot/greenshot/issues/1060

src/Greenshot.Editor/Configuration/LanguageKeys.cs
@@ -61,6 +61,7 @@ namespace Greenshot.Editor.Configuration
         settings_destination,
         settings_destination_clipboard,
         settings_destination_editor,
+        settings_destination_editor_add,
         settings_destination_fileas,
         settings_destination_printer,
         settings_destination_picker,

src/Greenshot.Editor/Destinations/EditorDestination.cs
@@ -65,8 +65,8 @@ namespace Greenshot.Editor.Destinations
                 }
 
                 var title = editor.CaptureDetails?.Title;
-                if (title == null) return Language.GetString(LangKey.settings_destination_editor);
-                return Language.GetString(LangKey.settings_destination_editor) + " - " + title.Substring(0, Math.Min(20, title.Length));
+                if (title == null) return Language.GetString(LangKey.settings_destination_editor_add);
+                return Language.GetString(LangKey.settings_destination_editor_add) + " - " + title.Substring(0, Math.Min(20, title.Length));
             }
         }
 

src/Greenshot.Editor/Forms/ImageEditorForm.cs
@@ -187,14 +187,18 @@ namespace Greenshot.Editor.Forms
             // Initial "saved" flag for asking if the image needs to be save
             _surface.Modified = !outputMade;
 
-            // Only register in the global editor list after the surface is fully assigned,
-            // so background threads iterating Editors never see a partially-initialized editor.
-            lock (_editorListLock)
-            {
-                EditorList.Add(this);
-            }
+            // Note: SetSurface (called via Surface = surface above) already registered this
+            // editor in EditorList. Do NOT add again here — double-registration causes
+            // closed editors to linger in the list because Remove() only removes one entry.
 
             UpdateUi();
+
+            // Re-apply the capture title after UpdateUi()/ApplyLanguage() which resets Text
+            // to just the bare form language key ("Greenshot editor").
+            if (_surface?.CaptureDetails?.Title != null)
+            {
+                Text = _surface.CaptureDetails.Title + " - " + Language.GetString(LangKey.editor_title);
+            }
 
             // Workaround: for the MouseWheel event which doesn't get to the panel
             MouseWheel += PanelMouseWheel;

src/Greenshot/Languages/language-en-US.xml
@@ -238,6 +238,7 @@ Please check write accessibility of the selected storage location.</resource>
 		<resource name="settings_destination">Destination</resource>
 		<resource name="settings_destination_clipboard">Copy to clipboard</resource>
 		<resource name="settings_destination_editor">Open in image editor</resource>
+		<resource name="settings_destination_editor_add">Add to editor</resource>
 		<resource name="settings_destination_email">E-Mail</resource>
 		<resource name="settings_destination_file">Save directly (using settings below)</resource>
 		<resource name="settings_destination_fileas">Save as (displaying dialog)</resource>